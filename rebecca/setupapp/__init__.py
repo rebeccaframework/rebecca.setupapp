814f3f57ac459b0442d817827dafc6b1f1403a2b
Atsushi Odagiri <aodagx@gmail.com>
2011-11-05 01:53:55 -0700

    garden

--- rebecca/setupapp/__init__.py
+++ rebecca/setupapp/__init__.py
@@ -1,4 +1,51 @@
+
+import inspect
+from argparse import ArgumentParser
+
 from .directives import add_setup
+from .util import get_setup_commands, get_setup_command
+
+class SetupApp(object):
+    def __init__(self, registry):
+        self.registry = registry
+
+    @property
+    def command_names(self):
+        return list(c[0] for c in get_setup_commands(self.registry))
+
+
+    def __call__(self, command_name, args):
+        command = get_setup_command(self.registry, command_name)
+        argspec = inspect.getargspec(command)
+        defaults = {}
+        if argspec[3] is not None:
+            defaults = dict(zip(reversed(argspec[0]), reversed(argspec[3])))
+        command_args = [(n, getattr(args, n) if hasattr(args, n) else defaults[n]) 
+                for n in argspec[0]]
+        command_args = dict(command_args)
+        command(**command_args)
+
+    def make_parser(self):
+        parser = ArgumentParser()
+
+        self.add_commands(parser)
+
+        return parser
+
+    def add_commands(self, parser):
+        parsers = parser.add_subparsers()
+        for name, c in get_setup_commands(self.registry):
+            argspec = inspect.getargspec(c)
+            defaults = {}
+            if argspec[3] is not None:
+                defaults = dict(zip(reversed(argspec[0]), reversed(argspec[3])))
+            sub = parsers.add_parser(name, help=c.__doc__)
+            for n in argspec[0]:
+                if n not in defaults:
+                    sub.add_argument(n)
+                else:
+                    sub.add_argument('--' + n, default=defaults[n])
+            sub.set_defaults(command_name=name)
 
 def includeme(config):
     config.add_directive('add_setup', add_setup)
--- rebecca/setupapp/commands.py
+++ rebecca/setupapp/commands.py
@@ -1,45 +1,6 @@
 import sys
-from argparse import ArgumentParser
 from pyramid.paster import bootstrap
-import inspect
-
-from .util import get_setup_commands, get_setup_command
-
-class SetupApp(object):
-    def __init__(self, registry):
-        self.registry = registry
-
-    @property
-    def command_names(self):
-        return list(c[0] for c in get_setup_commands(self.registry))
-
-
-    def __call__(self, command_name):
-        command = get_setup_command(self.registry, command_name)
-        command()
-
-    def make_parser(self):
-        parser = ArgumentParser()
-
-        self.add_commands(parser)
-
-        return parser
-
-    def add_commands(self, parser):
-        parsers = parser.add_subparsers()
-        for name, c in get_setup_commands(self.registry):
-            argspec = inspect.getargspec(c)
-            defaults = {}
-            if argspec[3] is not None:
-                defaults = dict(zip(reversed(argspec[0]), reversed(argspec[3])))
-            sub = parsers.add_parser(name, help=c.__doc__)
-            for n in argspec[0]:
-                if n not in defaults:
-                    sub.add_argument(n)
-                else:
-                    sub.add_argument('--' + n, default=defaults[n])
-            sub.set_defaults(command_name=name)
-
+from . import SetupApp
 
 def main(args=sys.argv[1:]):
     inifile, args = args[0], args[1:]
@@ -49,7 +10,7 @@ def main(args=sys.argv[1:]):
         setup_app = SetupApp(app['registry'])
         parser = setup_app.make_parser()
         args = parser.parse_args(args)
-        setup_app(args.command_name)
+        setup_app(args.command_name, args)
     finally:
         closer()
 
